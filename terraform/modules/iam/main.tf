34ad27fcae24b64981f77f32c0f95b2901977abb
aashishkun12 <aashishkunwar1201@gmail.com>
2026-09-22 12:05:36 +0545

    add ALB controller role + policy

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,3 +1,5 @@
+// Cluster Role
+
 resource "aws_iam_role" "foodwaste_dev_eks_cluster_role" {
   name = "foodwaste_dev_eks_cluster_role"
   assume_role_policy = jsonencode({
@@ -19,6 +21,10 @@ resource "aws_iam_role_policy_attachment" "foodwaste_dev_eks_cluster_role_policy
   role = aws_iam_role.foodwaste_dev_eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+
+
+//Node ROle 
 
 resource "aws_iam_role" "foodwaste_dev_eks_node_role" {
   name = "foodwaste_dev_eks_node_role"
@@ -50,4 +56,38 @@ resource "aws_iam_role_policy_attachment" "foodwaste_dev_eks_node_EKSCNIPolicy" 
 resource "aws_iam_role_policy_attachment" "foodwaste_dev_eks_node_AmazonEC2ConRegReadOnly" {
   role = aws_iam_role.foodwaste_dev_eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+
+// ALB controller role + policy
+
+data "http" "alb_controller_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v3.5.0/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "foodwaste-dev-alb-controller-policy"
+  policy = data.http.alb_controller_iam_policy.response_body
+}
+
+resource "aws_iam_role" "alb_controller" {
+  name = "foodwaste-dev-alb-controller-role"
+
+  // Pod Identity trust policy - trusts the EKS Pod Identity service, not OIDC.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "pods.eks.amazonaws.com" }
+        Action    = ["sts:AssumeRole", "sts:TagSession"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
 }
--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,7 +1,15 @@
 output "foodwaste_eks_cluster_role_arn" {
+  description = "ARN of the IAM role used by the EKS control plane"
   value = aws_iam_role.foodwaste_dev_eks_cluster_role.arn
 }
 
 output "foodwaste_eks_worker_role_arn" {
+  description = "ARN of the IAM role used by EKS worker nodes"
   value = aws_iam_role.foodwaste_dev_eks_node_role.arn
+}
+
+
+output "alb_controller_role_arn" {
+  description = "ARN of the IAM role used by the AWS Load Balancer Controller"
+  value       = aws_iam_role.alb_controller.arn
 }
